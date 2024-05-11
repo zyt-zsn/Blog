abf8dd22635a6dea7a8273311111aff2e9b921b9
zyt-zsn <zyt_xl@163.com>
2024-05-11 15:18:35 +0800

    Update build-site.el

--- build-site.el
+++ build-site.el
@@ -1,37 +1,44 @@
 (require 'ox-publish)
 (require 'package)
-(require 'files)
-;; Define the publishing project
 (let* (
-	   (file (buffer-file-name))
-	   (dir (file-name-parent-directory file))
+	   (file load-file-name)
+	   (dir (file-name-directory (directory-file-name file)))
 	   (package-user-dir (concat dir ".packages"))
-	   (org-publish-project-alist
-		(list
-		 (list "my-org-site"
-			   :recursive t
-			   :author "zytbit"
-			   :email "zytbit@gmail.com"
-			   :with-email t
-			   :base-directory (file-name-concat dir "content")
-			   :publishing-directory (file-name-concat dir "public")
-			   :publishing-function 'org-html-publish-to-html)))
-	   org-html-validation-link
-	   org-html-head-include-default-style
-	   org-html-head-include-default-style
-	   (org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
-	   (httpd-root (file-name-concat dir "public"))
-	   (httpd-port "8080")
+	   (package-archives '(
+						   ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")  ;;zyt
+						   ("elpa" . "https://elpa.gnu.org/packages/")
+						   ("org" . "https://orgmode.org/elpa/")
+						   ))
 	   )
   (package-initialize)
   (unless package-archive-contents
 	(package-refresh-contents))
   (package-install 'htmlize)
   (package-install 'simple-httpd)
-  ;; Generate the site output
-  (org-publish-all t)
-  (httpd-start)
-  ;; (browse-url--browser (concat "http://localhost:" httpd-port)) 
-  (eww-browse-url (concat "http://localhost:" httpd-port)) 
+  ;; Define the publishing project
+  (let* (
+		 (org-publish-project-alist
+		  (list
+		   (list "my-org-site"
+				 :recursive t
+				 :author "zytbit"
+				 :email "zytbit@gmail.com"
+				 :with-email t
+				 :base-directory (file-name-concat dir "content")
+				 :publishing-directory (file-name-concat dir "public")
+				 :publishing-function 'org-html-publish-to-html)))
+		 org-html-validation-link
+		 org-html-head-include-default-style
+		 org-html-head-include-default-style
+		 (org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+		 (httpd-root (file-name-concat dir "public"))
+		 (httpd-port "8080")
+		 )
+	;; Generate the site output
+	(org-publish-all t)
+	;; (httpd-start)
+	;; ;; (browse-url--browser (concat "http://localhost:" httpd-port)) 
+	;; (eww-browse-url (concat "http://localhost:" httpd-port)) 
+	)
   )
 (message "Build complete!")
