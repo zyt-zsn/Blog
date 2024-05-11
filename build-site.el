4c7ad8603b89f901e6279fc782e5d87e231d43ee
zyt-zsn <zyt_xl@163.com>
2024-05-11 11:52:45 +0800

    first commit

---
+++ build-site.el
@@ -0,0 +1,36 @@
+(require 'ox-publish)
+(require 'simple-httpd)
+(require 'package)
+;; Define the publishing project
+(let* (
+	   (file (buffer-file-name))
+	   (dir (file-name-parent-directory file))
+	   (package-user-dir (concat dir ".packages"))
+	   (org-publish-project-alist
+		(list
+		 (list "my-org-site"
+			   :recursive t
+			   :author "zytbit"
+			   :email "zytbit@gmail.com"
+			   :with-email t
+			   :base-directory (file-name-concat dir "content")
+			   :publishing-directory (file-name-concat dir "public")
+			   :publishing-function 'org-html-publish-to-html)))
+	   org-html-validation-link
+	   org-html-head-include-default-style
+	   org-html-head-include-default-style
+	   (org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+	   (httpd-root (file-name-concat dir "public"))
+	   (httpd-port "8080")
+	   )
+  (package-initialize)
+  (unless package-archive-contents
+	(package-refresh-contents))
+  (package-install 'htmlize)
+  ;; Generate the site output
+  (org-publish-all t)
+  (httpd-start)
+  ;; (browse-url--browser (concat "http://localhost:" httpd-port)) 
+  (eww-browse-url (concat "http://localhost:" httpd-port)) 
+  )
+(message "Build complete!")
